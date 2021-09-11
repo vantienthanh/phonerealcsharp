7ec5bd7cf8a88d67208f4579a57bb9c9c93a484d
Thanh Van <vantienthanh123@gmail.com>
2021-09-11 23:46:01 +0700

    lasted commit

--- phone/Form1.Designer.cs
+++ phone/Form1.Designer.cs
@@ -244,6 +244,7 @@ namespace phone
             this.Controls.Add(this.btnSendRequest);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
